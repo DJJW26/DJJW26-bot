2f9a2083a295f2169087fb4160f64a02adf96b95
DJJW26 <divijjjw@gmail.com>
2022-05-16 15:27:38 +0530

    first commit

---
+++ commands/afk.js
@@ -0,0 +1,60 @@
+const db = require('../models/afkModel')
+
+module.exports = {
+    name: 'afk',
+    async execute(message, args, client) {
+        const admin = message.member.permissions.has('ADMINISTRATOR')
+        if (['clear', 'remove'].includes(args[0]) && admin) {
+            args.shift()
+            if (!args[0])
+                return message.channel.send('Please provide a user')
+
+            const mention =
+                message.mentions.members.size > 0
+                    ? message.mentions.members.first()
+                    : message.guild.members.cache.get(args[0]) || null
+
+            if (!mention) return message.channel.send('Couldnt find the user');
+
+            const dbUser = db.findOne({ User: mention.id })
+            if (!dbUser || dbUser.afk.afk == false) return message.channel.send('User is not afk')
+
+            dbUser.afk.afk = false;
+            dbUser.save()
+            message.react('☑️')
+        }
+
+        let user = await db.findOne({ userId: message.author.id })
+        if (!user) {
+            const newUser = new db({
+                User: message.author.id,
+                afk: {
+                    afk: true,
+                    reason: 'AFK',
+                },
+            })
+            newUser.save()
+            user = newUser
+        }
+
+        const reason = args.join(' ') || 'AFK'
+
+        user.afk = {
+            afk: true,
+            reason: reason,
+            time: new Date()
+        }
+        user.save()
+
+        message.channel.send(`${message.member}, I have set your afk:- \n ${reason}`)
+        try {
+            message.member.setNickname(`${message.member.displayName} ~ AFK`)
+        } catch (err) {
+            console.log(err)
+        }
+        setTimeout(() => {
+            client.afkInfo.afks.push(message.author.id)
+        }, 5000)
+        return
+    }
+}
---
+++ commands/ocr.js
@@ -0,0 +1,24 @@
+const tesseract = require('tesseract.js');
+const {createWorker} = require('tesseract.js');
+module.exports = {
+    name: 'ocr',
+    aliases: ['image-to-text', 'itt'],
+    async execute(message, args, client, Discord){
+        const img = message.attachments.first();
+        if(!img) return message.reply('Please provide an image.')
+
+        try {
+            const worker = createWorker();
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            const {
+                data: { text },
+            } =  await worker.recognize(img.url);
+            await worker.terminate()
+            message.reply(text)
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
