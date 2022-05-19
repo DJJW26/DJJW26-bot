f3aee23f449c3bd98dacb1354347d3e954ad4a27
DJJW26 <divijjjw@gmail.com>
2022-05-19 21:24:51 +0530

    first commit

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -1,4 +1,4 @@
-module.exports = async (oldMessage, newMessage, Discord, client) => {
+module.exports = async (Discord, client, oldMessage, newMessage) => {
     if (newMessage.author.bot) return
     let snipes = client.snipes.esnipes.get(oldMessage.channel.id) || []
 
