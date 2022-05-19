40aa12cdae6d9b010036d28e1c13f4e12b08e4c6
DJJW26 <divijjjw@gmail.com>
2022-05-19 21:20:00 +0530

    first commit

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -1,15 +1,15 @@
 module.exports = async (oldMessage, newMessage, Discord, client) => {
     if (newMessage.author.bot) return
-        let snipes = client.snipes.esnipes.get(oldMessage.channel.id) || []
+    let snipes = client.snipes.esnipes.get(oldMessage.channel.id) || []
 
-        snipes.unshift({
-            oldContent: oldMessage.content,
-            newContent: newMessage.content,
-            editedIn: newMessage.createdAt - oldMessage.editedAt,
-            member: newMessage.member,
-            author: newMessage.author,
-            msg: newMessage,
-        })
+    snipes.unshift({
+        oldContent: oldMessage.content,
+        newContent: newMessage.content,
+        editedIn: newMessage.createdAt - oldMessage.editedAt,
+        member: newMessage.member,
+        author: newMessage.author,
+        msg: newMessage,
+    })
 
-        client.snipes.esnipes.set(oldMessage.channel.id, snipes)
+    client.snipes.esnipes.set(oldMessage.channel.id, snipes)
 }
