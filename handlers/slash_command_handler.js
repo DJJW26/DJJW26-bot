dc6f887cd92f3e6462e798fb2c39fb9c1fa0081e
DJJW26 <divijjjw@gmail.com>
2022-06-13 08:16:52 +0530

    caution !! work in progress

--- handlers/slash_command_handler.js
+++ handlers/slash_command_handler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = (client, Discord) => {
     const commandFiles = fs
-        .readdirSync('./slashcommands/')
+        .readdirSync('../slash_commands/')
         .filter((file) => file.endsWith('.js'))
     for (const file of commandFiles) {
         const command = require(`../slashcommands/${file}`)
--- main.js
+++ main.js
@@ -12,7 +12,7 @@ client.commands = new Discord.Collection();
 client.slashCommands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-['command_handler', 'event_handler'].forEach(handler => {
+['command_handler', 'slash_command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord)
 });
 
