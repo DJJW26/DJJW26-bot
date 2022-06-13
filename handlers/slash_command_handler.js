89e45920cba0105dcbc55d06ea48ec6a435caaa5
DJJW26 <divijjjw@gmail.com>
2022-06-13 09:31:31 +0530

    slash commands are ready :)

--- handlers/slash_command_handler.js
+++ handlers/slash_command_handler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = (client, Discord) => {
     const commandFiles = fs
-        .readdirSync('../slash_commands/')
+        .readdirSync('./slash_commands/')
         .filter((file) => file.endsWith('.js'))
     for (const file of commandFiles) {
         const command = require(`../slash_commands/${file}`);
