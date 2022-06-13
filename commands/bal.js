7f3556ce4de241d1739398d0fb8d380c97fdb443
DJJW26 <divijjjw@gmail.com>
2022-06-13 12:29:48 +0530

    i give up just use mentions

--- commands/bal.js
+++ commands/bal.js
@@ -1,4 +1,4 @@
-const {MessageEmbed} = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const profileModel = require(`./../models/profileSchema`)
 
 module.exports = {
@@ -9,49 +9,34 @@ module.exports = {
     description: 'Lets you check your balance',
     category: 'economy',
     async execute(message, args) {
-        let userBal = null;
-        let userBal1 = null;
-        if (args.length > 0) {
-            if (args[0].startsWith('<@')) {
-                if (args[0].startsWith('<@&')) {
-                }
-                else {
-                    userBal = message.mentions.users.first();
-                }
-            }
+        var user = null;
+
+        if (message.mentions.users.size) {
+            user = message.mentions.users.first().id;
+        } else {
+            user = message.author.id;
         }
-        else {
-            userBal = message.author.id;
-            userBal1 = message.author;
-        }
+
         try {
-            ProfileData = await profileModel.findOne({ userID: userBal })
-            if (!ProfileData) {
-                let profile = await profileModel.create({
-                    userID: { type: String, required: true },
-                    coins: { type: Number, default: 5000, min: 0 },
-                    bank: { type: Number, default: 0, min: 0 },
-                    inventory: [Item]
-                })
-                profile.save();
+            var profile = await profileModel.findOne({ userID: user });
+            if (!profile) {
+                const newProfile = new profileModel({
+                    userID: user,
+                    coins: 5000,
+                    bank: 0
+                });
+                await newProfile.save();
             }
-            ProfileData = await profileModel.findOne({ userID: userBal })
+            profile = await profileModel.findOne({ userID: user });
+            const embed = new MessageEmbed()
+                .setTitle('Balance')
+                .setColor('GREEN')
+                .addField('WALLET :', `${profile.coins.toLocaleString('en-US')}`)
+                .addField('BANK :', `${profile.bank.toLocaleString('en-US')}`)
+
+            return message.channel.send({ embeds: [embed] });
         } catch (err) {
             console.log(err)
         }
-
-        let wallet = ProfileData.coins.toLocaleString('en-US')
-        let bank = ProfileData.bank.toLocaleString('en-US')
-
-        const balEmbed = new MessageEmbed()
-            .setTitle(`**${userBal1.username}** bal`)
-            .setColor('GREEN')
-            .setThumbnail(`${userBal1.displayAvatarURL()}`)
-            .addField('WALLET :', `${wallet}`)
-            .addField('BANK :', `${bank}`)
-            .setFooter('what a scrub')
-
-        message.channel.send({ embeds: [balEmbed] });
     }
-
 }
